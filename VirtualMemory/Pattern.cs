e2b65b5318d9f5ae64264a310a874e610c715a9e
Даниил <D3l0ry@outlook.com>
2020-03-12 14:39:06 +0500

    Added a General function

--- VirtualMemory/Pattern.cs
+++ VirtualMemory/Pattern.cs
@@ -9,28 +9,12 @@ namespace NativeManager.VirtualMemory
 {
     public unsafe static class Pattern
     {
-        public static IntPtr FindPattern(HMemory memory, uint module, string pattern, int offset = 0)
+        public static IntPtr FindPattern(HMemory memory, uint module, byte[] pattern, int offset = 0)
         {
-            if(string.IsNullOrWhiteSpace(pattern))
+            if (pattern == null)
             {
                 return IntPtr.Zero;
             }
-
-            List<byte> Patterns = new List<byte>();
-
-            pattern.Split(' ').All((X) =>
-            {
-                if (X.Equals("?"))
-                {
-                    Patterns.Add(0);
-                }
-                else
-                {
-                    Patterns.Add(Convert.ToByte(X, 16));
-                }
-
-                return true;
-            });
 
             Kernel32.GetModuleInformation(memory.Handle, (IntPtr)module, out MODULEINFO MODULEINFO, sizeof(MODULEINFO));
 
@@ -38,9 +22,9 @@ namespace NativeManager.VirtualMemory
             {
                 bool Found = true;
 
-                for (int MIndex = 0; MIndex < Patterns.Count; MIndex++)
+                for (int MIndex = 0; MIndex < pattern.Length; MIndex++)
                 {
-                    Found = Patterns[MIndex] == 0 || memory.Read<byte>(PIndex + MIndex) == Patterns[MIndex];
+                    Found = pattern[MIndex] == 0 || memory.Read<byte>((uint)(PIndex + MIndex)) == pattern[MIndex];
 
                     if (!Found)
                     {
@@ -56,5 +40,79 @@ namespace NativeManager.VirtualMemory
 
             return IntPtr.Zero;
         }
+
+        public static IntPtr FindPattern(HMemory memory, uint module, string pattern, int offset = 0)
+        {
+            if (string.IsNullOrWhiteSpace(pattern))
+            {
+                return IntPtr.Zero;
+            }
+
+            List<byte> patterns = new List<byte>();
+
+            pattern.Split(' ').All((X) =>
+            {
+                if (X.Equals("?"))
+                {
+                    patterns.Add(0);
+                }
+                else
+                {
+                    patterns.Add(Convert.ToByte(X, 16));
+                }
+
+                return true;
+            });
+
+            return FindPattern(memory, module, patterns.ToArray(), offset);
+        }
+
+        //pu}blic static IntPtr FindPattern(HMemory memory, uint module, string pattern, int offset = 0)
+        //{
+        //    if(string.IsNullOrWhiteSpace(pattern))
+        //    {
+        //        return IntPtr.Zero;
+        //    }
+
+        //    List<byte> Patterns = new List<byte>();
+
+        //    pattern.Split(' ').All((X) =>
+        //    {
+        //        if (X.Equals("?"))
+        //        {
+        //            Patterns.Add(0);
+        //        }
+        //        else
+        //        {
+        //            Patterns.Add(Convert.ToByte(X, 16));
+        //        }
+
+        //        return true;
+        //    });
+
+        //    Kernel32.GetModuleInformation(memory.Handle, (IntPtr)module, out MODULEINFO MODULEINFO, sizeof(MODULEINFO));
+
+        //    for (long PIndex = module; PIndex < MODULEINFO.SizeOfImage; PIndex++)
+        //    {
+        //        bool Found = true;
+
+        //        for (int MIndex = 0; MIndex < Patterns.Count; MIndex++)
+        //        {
+        //            Found = Patterns[MIndex] == 0 || memory.Read<byte>((uint)(PIndex + MIndex)) == Patterns[MIndex];
+
+        //            if (!Found)
+        //            {
+        //                break;
+        //            }
+        //        }
+
+        //        if (Found)
+        //        {
+        //            return (IntPtr)(PIndex + offset);
+        //        }
+        //    }
+
+        //    return IntPtr.Zero;
+        //}
     }
 }
