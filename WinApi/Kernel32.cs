abcf43f9c76e349eb7449d15d94c055d78388a05
Даниил <D3l0ry@outlook.com>
2020-03-18 15:31:49 +0500

    Optimization functions

--- WinApi/Kernel32.cs
+++ WinApi/Kernel32.cs
@@ -10,41 +10,59 @@ namespace NativeManager.WinApi
     [SuppressUnmanagedCodeSecurity]
     public static unsafe class Kernel32
     {
-        [DllImport("kernel32.dll", SetLastError = true)]
+        private const string m_KernelDll = "kernel32.dll";
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
         public static extern IntPtr OpenProcess(ProcessAccess processAccess, bool bInheritHandle, int processId);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool CloseHandle(IntPtr hObject);
 
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true, ExactSpelling = true)]
         public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
 
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true, ExactSpelling = true)]
         public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, AllocationType dwFreeType);
 
-        [DllImport("kernel32.dll")]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll)]
         public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress,UIntPtr dwSize, ProtectCode flNewProtect, out ProtectCode lpflOldProtect);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, void* lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesRead);
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, IntPtr lpNumberOfBytesRead);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(IntPtr hProcess, void* lpBaseAddress,[In] byte[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,[In] byte[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(IntPtr hProcess, void* lpBaseAddress, [In] void* lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In] IntPtr lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
 
-        [DllImport("kernel32.dll")]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll)]
         public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, SetLastError = true)]
         public static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
 
+        [SuppressUnmanagedCodeSecurity]
         [DllImport("psapi.dll", SetLastError = true)]
         public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, out MODULEINFO lpmodinfo, int cb);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        public static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, CharSet = CharSet.Ansi, ExactSpelling = false, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_KernelDll, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
--- WinApi/User32.cs
+++ WinApi/User32.cs
@@ -1,18 +1,22 @@
-﻿using NativeManager.WinApi.Enums;
-
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Security;
+
+using NativeManager.WinApi.Enums;
 
 namespace NativeManager.WinApi
 {
     [SuppressUnmanagedCodeSecurity]
     public static class User32
     {
-        [DllImport("user32.dll")]
+        private const string m_UserDll = "user32.dll";
+
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_UserDll)]
         public static extern bool GetAsyncKeyState(KeysCode vKey);
 
-        [DllImport("user32.dll")]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport(m_UserDll)]
         public static extern IntPtr GetForegroundWindow();
     }
 }
