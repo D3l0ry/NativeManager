826a204c45c0057f399263263c48670ad089b89c
Даниил <D3l0ry@outlook.com>
2020-05-12 00:04:20 +0500

    Added constructor and optimization code

--- MemoryInteraction/SimpleMemoryManager.cs
+++ MemoryInteraction/SimpleMemoryManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-
+using System.Runtime.InteropServices;
 using NativeManager.MemoryInteraction.Interfaces;
 using NativeManager.ProcessInteraction;
 using NativeManager.WinApi;
@@ -8,31 +8,32 @@ using NativeManager.WinApi.Enums;
 
 namespace NativeManager.MemoryInteraction
 {
-    public class SimpleMemoryManager:ISimpleMemory
+    public class SimpleMemoryManager : ISimpleMemory
     {
-        public IntPtr Handle { get; private set; }
+        protected readonly IntPtr m_Handle;
 
         public Process SelectedProcess { get; private set; }
 
-        public SimpleMemoryManager(Process process, ProcessAccess access = ProcessAccess.ALL)
+        public SimpleMemoryManager(Process process)
         {
-            Handle = Kernel32.OpenProcess(access, false, process.Id);
-
-            if (Handle == IntPtr.Zero)
-            {
-                throw new NullReferenceException("Failed to open process descriptor");
-            }
-
             SelectedProcess = process;
+            m_Handle = process.Handle;
         }
 
-        public SimpleMemoryManager(string processName, int index = 0, ProcessAccess access = ProcessAccess.ALL):this(ProcessInfo.Exists(Process.GetProcessesByName(processName), index), access) { }
+        public SimpleMemoryManager(string processName, int index = 0) : this(ProcessInfo.Exists(Process.GetProcessesByName(processName), index)) { }
+
+        public SimpleMemoryManager(int processId) : this(Process.GetProcessById(processId)) { }
+
+        ~SimpleMemoryManager()
+        {
+            Dispose();
+        }
 
         public virtual byte[] ReadBytes(IntPtr address, int size)
         {
             byte[] buffer = new byte[size];
 
-            if (Kernel32.ReadProcessMemory(Handle, address, buffer, size, IntPtr.Zero))
+            if (Kernel32.ReadProcessMemory(m_Handle, address, buffer, size, IntPtr.Zero))
             {
                 return buffer;
             }
@@ -40,6 +41,8 @@ namespace NativeManager.MemoryInteraction
             return buffer;
         }
 
-        public virtual bool WriteBytes(IntPtr address, byte[] buffer) => Kernel32.WriteProcessMemory(Handle, address, buffer, buffer.Length, IntPtr.Zero);
+        public virtual bool WriteBytes(IntPtr address, byte[] buffer) => Kernel32.WriteProcessMemory(m_Handle, address, buffer, buffer.Length, IntPtr.Zero);
+
+        public void Dispose() => Kernel32.CloseHandle(m_Handle);
     }
 }
