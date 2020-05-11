66b174c1f6de29af7d107bb4b842a01f4573296c
Даниил <D3l0ry@outlook.com>
2020-05-12 00:02:40 +0500

    Added methods Reset and Undo

--- MemoryInteraction/Allocator.cs
+++ MemoryInteraction/Allocator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using NativeManager.MemoryInteraction.Interfaces;
 using NativeManager.WinApi;
 using NativeManager.WinApi.Enums;
@@ -12,10 +11,14 @@ namespace NativeManager.MemoryInteraction
 
         public Allocator(IMemory memory) => m_Memory = memory;
 
-        public IntPtr Alloc(uint size, AllocationType allocationType = AllocationType.MEM_COMMIT | AllocationType.MEM_RESERVE, MemoryProtection memoryProtection = MemoryProtection.PAGE_EXECUTE_READWRITE) => Kernel32.VirtualAllocEx(m_Memory.Handle, IntPtr.Zero, size, allocationType, memoryProtection);
+        public IntPtr Alloc(uint size, MemoryProtection memoryProtection = MemoryProtection.PAGE_EXECUTE_READWRITE) => Kernel32.VirtualAllocEx(m_Memory.SelectedProcess.Handle, IntPtr.Zero, size, AllocationType.MEM_COMMIT | AllocationType.MEM_RESERVE, memoryProtection);
 
-        public bool Free(IntPtr address) => Kernel32.VirtualFreeEx(m_Memory.Handle, address, 0, FreeType.MEM_RELEASE);
+        public IntPtr Reset(IntPtr address, uint size) => Kernel32.VirtualAllocEx(m_Memory.SelectedProcess.Handle, address, size, AllocationType.MEM_RESET, MemoryProtection.PAGE_NOACCESS);
 
-        public bool Protect(IntPtr address, uint size, AllocationProtect protectCode, out AllocationProtect oldProtect) => Kernel32.VirtualProtectEx(m_Memory.Handle, address, (UIntPtr)size, protectCode, out oldProtect);
+        public IntPtr Undo(IntPtr address, uint size) => Kernel32.VirtualAllocEx(m_Memory.SelectedProcess.Handle, address, size, AllocationType.MEM_RESET_UNDO, MemoryProtection.PAGE_NOACCESS);
+
+        public bool Free(IntPtr address) => Kernel32.VirtualFreeEx(m_Memory.SelectedProcess.Handle, address, 0, FreeType.MEM_RELEASE);
+
+        public bool Protect(IntPtr address, uint size, AllocationProtect protectCode, out AllocationProtect oldProtect) => Kernel32.VirtualProtectEx(m_Memory.SelectedProcess.Handle, address, (UIntPtr)size, protectCode, out oldProtect);
     }
 }
