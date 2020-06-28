89cac5d464add96f7b0badf63110f3d6d8476149
Даниил <D3l0ry@outlook.com>
2020-06-28 20:32:39 +0500

    Removed methods (Converter)

--- MemoryInteraction/Executor.cs
+++ MemoryInteraction/Executor.cs
@@ -1,89 +1,27 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.WinApi;
 
-using NativeManager.MemoryInteraction.Interfaces;
-using NativeManager.WinApi;
-
-namespace NativeManager.MemoryInteraction
+namespace System.MemoryInteraction
 {
     public unsafe sealed class Executor
     {
+        private readonly Process m_Process;
         private readonly IMemory m_Memory;
 
-        public Executor(IMemory memory) => m_Memory = memory;
+        public Executor(Process process, IMemory memory)
+        {
+            m_Process = process;
+            m_Memory = memory;
+        }
 
         public bool Execute(IntPtr address, IntPtr args)
         {
-            IntPtr thread = Kernel32.CreateRemoteThread(m_Memory.SelectedProcess.Handle, IntPtr.Zero, 0, address, args, 0, IntPtr.Zero);
+            IntPtr thread = Kernel32.CreateRemoteThread(m_Process.Handle, IntPtr.Zero, IntPtr.Zero, address, args, IntPtr.Zero, IntPtr.Zero);
 
             Kernel32.WaitForSingleObject(thread, 0xFFFFFFFF);
             Kernel32.CloseHandle(thread);
 
             return thread != IntPtr.Zero;
-        }
-
-        public static byte[] StructureToByte<T>(ref T structure) where T : unmanaged
-        {
-            int length = Marshal.SizeOf<T>();
-
-            byte[] array = new byte[length];
-
-            fixed (void* arrayPtr = array)
-            {
-                *(T*)arrayPtr = structure;
-            }
-
-            return array;
-        }
-
-        public static byte[] StructureToByte<T>(T structure) where T : unmanaged
-        {
-            int length = Marshal.SizeOf<T>();
-
-            byte[] array = new byte[length];
-
-            fixed (void* arrayPtr = array)
-            {
-                *(T*)arrayPtr = structure;
-            }
-
-            return array;
-        }
-
-        public static T ByteToStructure<T>(ref byte[] bytes) where T : unmanaged
-        {
-            if (bytes == null)
-            {
-                throw new ArgumentNullException("bytes");
-            }
-
-            if (bytes.Length < Marshal.SizeOf<T>())
-            {
-                throw new ArgumentOutOfRangeException("bytes", "bytes length smaller than the size of the structure");
-            }
-
-            fixed (byte* bytesPtr = bytes)
-            {
-                return *(T*)bytesPtr;
-            }
-        }
-
-        public static T ByteToStructure<T>(byte[] bytes) where T : unmanaged
-        {
-            if (bytes == null)
-            {
-                throw new ArgumentNullException("bytes");
-            }
-
-            if (bytes.Length < Marshal.SizeOf<T>())
-            {
-                throw new ArgumentOutOfRangeException("bytes", "bytes length smaller than the size of the structure");
-            }
-
-            fixed (byte* bytesPtr = bytes)
-            {
-                return *(T*)bytesPtr;
-            }
         }
 
         public bool CallFunction(IntPtr address, ref byte[] args)
@@ -93,7 +31,7 @@ namespace NativeManager.MemoryInteraction
 
             try
             {
-                alloc = m_Memory.GetAllocator().Alloc((uint)args.Length);
+                alloc = m_Memory.GetAllocator().Alloc(args.Length);
 
                 if (alloc == IntPtr.Zero)
                 {
@@ -122,7 +60,7 @@ namespace NativeManager.MemoryInteraction
 
             try
             {
-                alloc = m_Memory.GetAllocator().Alloc((uint)args.Length);
+                alloc = m_Memory.GetAllocator().Alloc(args.Length);
 
                 if (alloc == IntPtr.Zero)
                 {
@@ -144,8 +82,8 @@ namespace NativeManager.MemoryInteraction
             return execute;
         }
 
-        public bool CallFunction<T>(IntPtr address, ref T args) where T : unmanaged => CallFunction(address, StructureToByte(ref args));
+        public bool CallFunction<T>(IntPtr address, ref T args) where T : unmanaged => CallFunction(address, GenericsConverter.StructureToBytes(ref args));
 
-        public bool CallFunction<T>(IntPtr address, T args) where T : unmanaged => CallFunction(address, StructureToByte(args));
+        public bool CallFunction<T>(IntPtr address, T args) where T : unmanaged => CallFunction(address, GenericsConverter.StructureToBytes(args));
     }
 }
