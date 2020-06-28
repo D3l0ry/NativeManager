b258b31f63539cba005eab219471df1a4d6dc7af
Даниил <D3l0ry@outlook.com>
2020-06-28 20:31:30 +0500

    Updated constructor and renamed params

--- MemoryInteraction/PatternManager.cs
+++ MemoryInteraction/PatternManager.cs
@@ -1,36 +1,28 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 
-using NativeManager.MemoryInteraction.Interfaces;
-using NativeManager.ProcessInteraction;
-
-namespace NativeManager.MemoryInteraction
+namespace System.MemoryInteraction
 {
     public unsafe class PatternManager
     {
+        private readonly Process m_Process;
         private readonly IMemory m_Memory;
 
-        public PatternManager(IMemory memory) => m_Memory = memory;
+        public PatternManager(Process process, IMemory memory)
+        {
+            m_Process = process;
+            m_Memory = memory;
+        }
 
         public IntPtr FindPattern(string module, byte[] pattern, int offset = 0)
         {
-            ProcessModule moduleInfo = m_Memory.SelectedProcess.GetModule(module);
+            ProcessModule moduleInfo = m_Process.GetModule(module);
 
             return FindPattern(moduleInfo, pattern, offset);
         }
 
         public IntPtr FindPattern(string module, string pattern, int offset = 0) => FindPattern(module, GetPattern(pattern), offset);
-
-        public IntPtr FindPattern(IntPtr modulePtr, byte[] pattern, int offset = 0)
-        {
-            ProcessModule moduleInfo = m_Memory.SelectedProcess.GetModule(modulePtr);
-
-            return FindPattern(moduleInfo, pattern, offset);
-        }
-
-        public IntPtr FindPattern(IntPtr modulePtr, string pattern, int offset = 0) => FindPattern(modulePtr, GetPattern(pattern), offset);
 
         public IntPtr FindPattern(ProcessModule moduleInfo, byte[] pattern, int offset = 0)
         {
@@ -39,21 +31,21 @@ namespace NativeManager.MemoryInteraction
                 throw new ArgumentNullException("moduleInfo");
             }
 
-            return FindPattern(moduleInfo.BaseAddress, pattern, moduleInfo.ModuleMemorySize, offset);
+            return FindPattern(moduleInfo.BaseAddress, (IntPtr)moduleInfo.ModuleMemorySize, pattern, offset);
         }
 
         public IntPtr FindPattern(ProcessModule moduleInfo, string pattern, int offset = 0) => FindPattern(moduleInfo, GetPattern(pattern), offset);
 
-        public virtual IntPtr FindPattern(IntPtr modulePtr, byte[] pattern, int searchSize, int offset)
+        public virtual IntPtr FindPattern(IntPtr startAddress,IntPtr endAddress, byte[] pattern, int offset)
         {
             if (pattern == null)
             {
                 throw new ArgumentNullException("pattern");
             }
 
-            long indexMax = modulePtr.ToInt64() + searchSize;
+            long indexMax = startAddress.ToInt64() + endAddress.ToInt64();
 
-            for (long PIndex = modulePtr.ToInt64(); PIndex < indexMax; PIndex++)
+            for (long PIndex = startAddress.ToInt64(); PIndex < indexMax; PIndex++)
             {
                 bool Found = true;
 
@@ -76,7 +68,7 @@ namespace NativeManager.MemoryInteraction
             return IntPtr.Zero;
         }
 
-        public IntPtr FindPattern(IntPtr modulePtr, string pattern, int searchSize, int offset) => FindPattern(modulePtr, GetPattern(pattern), searchSize, offset);
+        public IntPtr FindPattern(IntPtr startAddress, IntPtr endAddress, string pattern, int offset) => FindPattern(startAddress,endAddress, GetPattern(pattern), offset);
 
         private byte[] GetPattern(string pattern)
         {
