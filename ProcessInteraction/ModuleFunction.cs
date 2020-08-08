864da66455f7b4dd69e498ad9e5ebc276f3ba538
Даниил <D3l0ry@outlook.com>
2020-08-08 18:12:45 +0500

    Added ModuleFunction and ModuleCollection

---
+++ ProcessInteraction/ModuleFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Diagnostics
+{
+    public class ModuleFunction
+    {
+        internal ModuleFunction(string name, IntPtr virtualAddress)
+        {
+            Name = name;
+            VirtualAddress = virtualAddress;
+        }
+
+        public string Name;
+        public IntPtr VirtualAddress;
+    }
+}
---
+++ ProcessInteraction/ModuleFunctionCollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace System.Diagnostics
+{
+    public class ModuleFunctionCollection:ReadOnlyCollectionBase
+    {
+        public ModuleFunctionCollection(ModuleFunction[] moduleFunctions) => InnerList.AddRange(moduleFunctions);
+
+        public ModuleFunction this[int index] => InnerList[index] as ModuleFunction;
+
+        public ModuleFunction this[string name] => InnerList.Cast<ModuleFunction>().Where(X => X.Name == name).FirstOrDefault();
+    }
+}
