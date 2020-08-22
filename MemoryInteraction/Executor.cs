2fc78b9a54a4ebdef341b0e86043a3bae8e702f0
Даниил <D3l0ry@outlook.com>
2020-08-22 16:33:17 +0500

    Updated

--- MemoryInteraction/Executor.cs
+++ MemoryInteraction/Executor.cs
@@ -5,8 +5,8 @@ namespace System.MemoryInteraction
 {
     public unsafe sealed class Executor
     {
-        private readonly Process m_Process;
-        private readonly IMemory m_Memory;
+        private Process m_Process;
+        private IMemory m_Memory;
 
         public Executor(Process process, IMemory memory)
         {
@@ -33,32 +33,9 @@ namespace System.MemoryInteraction
 
         public static IntPtr GetFunction(string moduleName, string functionName)
         {
-            if(string.IsNullOrWhiteSpace(moduleName)) throw new ArgumentNullException(nameof(moduleName));
+            if (string.IsNullOrWhiteSpace(moduleName)) throw new ArgumentNullException(nameof(moduleName));
 
             return GetFunction(Kernel32.GetModuleHandle(moduleName), functionName);
-        }
-
-        public bool CallFunction(IntPtr address, ref byte[] args)
-        {
-            IntPtr alloc = IntPtr.Zero;
-            bool execute = false;
-
-            try
-            {
-                alloc = m_Memory.GetAllocator().Alloc(args.Length);
-
-                if (alloc == IntPtr.Zero) return false;
-
-                if (!m_Memory.WriteBytes(alloc, args)) return false;
-
-                execute = Execute(address, alloc);
-            }
-            finally
-            {
-                m_Memory.GetAllocator().Free(alloc);
-            }
-
-            return execute;
         }
 
         public bool CallFunction(IntPtr address, byte[] args)
@@ -70,9 +47,9 @@ namespace System.MemoryInteraction
             {
                 alloc = m_Memory.GetAllocator().Alloc(args.Length);
 
-                if (alloc == IntPtr.Zero) return false;
+                if (alloc == IntPtr.Zero) return execute;
 
-                if (!m_Memory.WriteBytes(alloc, args)) return false;
+                if (!m_Memory.WriteBytes(alloc, args)) return execute;
 
                 execute = Execute(address, alloc);
             }
@@ -84,8 +61,16 @@ namespace System.MemoryInteraction
             return execute;
         }
 
-        public bool CallFunction<T>(IntPtr address, ref T args) where T : unmanaged => CallFunction(address, GenericsConverter.StructureToBytes(ref args));
+        public bool CallFunction<T>(IntPtr address, ref T args) => CallFunction(address, GenericsConverter.ManagedToBytes(ref args));
 
-        public bool CallFunction<T>(IntPtr address, T args) where T : unmanaged => CallFunction(address, GenericsConverter.StructureToBytes(args));
+        public bool CallFunction<T>(IntPtr address, T args) => CallFunction(address, GenericsConverter.ManagedToBytes(args));
+
+        public void Dispose()
+        {
+            m_Process = null;
+            m_Memory = null;
+
+            GC.SuppressFinalize(this);
+        }
     }
 }
