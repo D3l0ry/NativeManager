561f27219d1c2e3c4a3da39ff2197677b4746ee5
Даниил <D3l0ry@outlook.com>
2020-10-04 17:40:18 +0500

    Update on Win32

--- WinApi/Enums/MEMORY_BASIC_INFORMATION.cs
+++ WinApi/Enums/MEMORY_BASIC_INFORMATION.cs
@@ -2,7 +2,6 @@
 
 namespace System.WinApi
 {
-#if WIN32
     [StructLayout(LayoutKind.Sequential)]
     public struct MEMORY_BASIC_INFORMATION
     {
@@ -14,19 +13,4 @@ namespace System.WinApi
         public AllocationProtect Protect;
         public MemoryType Type;
     }
-#elif WIN64
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MEMORY_BASIC_INFORMATION
-    {
-        public ulong BaseAddress;
-        public ulong AllocationBase;
-        public int AllocationProtect;
-        public int __alignment1;
-        public ulong RegionSize;
-        public int State;
-        public int Protect;
-        public int Type;
-        public int __alignment2;
-    }
-#endif
 }
