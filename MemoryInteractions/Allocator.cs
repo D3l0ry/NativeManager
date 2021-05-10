64f714f9fcbfd37994b5f15ec4b6e422f5799962
Даниил <D3l0ry@outlook.com>
2021-05-10 20:32:15 +0500

    Добавил перегрузку методов Alloc, Protect

--- MemoryInteractions/Allocator.cs
+++ MemoryInteractions/Allocator.cs
@@ -1,9 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.WinApi;
 
 namespace System.MemoryInteraction
 {
-    public sealed class Allocator : IAllocator
+    public sealed class Allocator
     {
         private Process m_Process;
 
@@ -12,6 +13,14 @@ namespace System.MemoryInteraction
         public IntPtr Alloc(int size) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, (IntPtr)size, AllocationType.MEM_COMMIT | AllocationType.MEM_RESERVE, MemoryProtection.PAGE_EXECUTE_READWRITE);
 
         public IntPtr Alloc(IntPtr size) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, size, AllocationType.MEM_COMMIT | AllocationType.MEM_RESERVE, MemoryProtection.PAGE_EXECUTE_READWRITE);
+
+        public IntPtr Alloc(int size, AllocationType allocationType) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, (IntPtr)size, allocationType, MemoryProtection.PAGE_EXECUTE_READWRITE);
+
+        public IntPtr Alloc(IntPtr size, AllocationType allocationType) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, size, allocationType, MemoryProtection.PAGE_EXECUTE_READWRITE);
+
+        public IntPtr Alloc(int size, AllocationType allocationType, MemoryProtection memoryProtection) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, (IntPtr)size, allocationType, memoryProtection);
+
+        public IntPtr Alloc(IntPtr size, AllocationType allocationType, MemoryProtection memoryProtection) => Kernel32.VirtualAllocEx(m_Process.Handle, IntPtr.Zero, size, allocationType, memoryProtection);
 
         public IntPtr Reset(IntPtr address, int size) => Kernel32.VirtualAllocEx(m_Process.Handle, address, (IntPtr)size, AllocationType.MEM_RESET, MemoryProtection.PAGE_NOACCESS);
 
@@ -23,9 +32,16 @@ namespace System.MemoryInteraction
 
         public bool Free(IntPtr address) => Kernel32.VirtualFreeEx(m_Process.Handle, address, IntPtr.Zero, FreeType.MEM_RELEASE);
 
-        public bool Protect(IntPtr address, int size, AllocationProtect protectCode, out AllocationProtect oldProtect) => Kernel32.VirtualProtectEx(m_Process.Handle, address, (IntPtr)size, protectCode, out oldProtect);
+        public AllocationProtect Protect(IntPtr address, IntPtr size, AllocationProtect protectCode)
+        {
+            Kernel32.VirtualProtectEx(m_Process.Handle, address, size, protectCode, out AllocationProtect oldProtect);
 
-        public bool Protect(IntPtr address, IntPtr size, AllocationProtect protectCode, out AllocationProtect oldProtect) => Kernel32.VirtualProtectEx(m_Process.Handle, address, size, protectCode, out oldProtect);
+            return oldProtect;
+        }
+
+        public AllocationProtect Protect(IntPtr address, int size, AllocationProtect protectCode) => Protect(address, (IntPtr)size, protectCode);
+
+        public AllocationProtect Protect<T>(IntPtr address, AllocationProtect protectCode) => Protect(address, Marshal.SizeOf<T>(), protectCode);
 
         public void Dispose()
         {
