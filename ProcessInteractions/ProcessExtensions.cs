8370bcef518a77b642b3f8110dbae0302bae03ee
Даниил Шантарин <d3l0ry@yandex.ru>
2022-12-14 14:47:40 +0500

    1)Перемещены структуры в отдельные файлы 2)Оптимизирован код

--- ProcessInteractions/ProcessExtensions.cs
+++ ProcessInteractions/ProcessExtensions.cs
@@ -1,118 +1,12 @@
 ﻿using System.Linq;
 using System.MemoryInteractions;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.WinApi;
 
 namespace System.Diagnostics
 {
-    public static unsafe class ProcessExtensions
+    public static unsafe partial class ProcessExtensions
     {
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_DOS_HEADER
-        {
-            readonly short e_magic;
-            readonly short e_cblp;
-            readonly short e_cp;
-            readonly short e_crlc;
-            readonly short e_cparhdr;
-            readonly short e_minalloc;
-            readonly short e_maxalloc;
-            readonly short e_ss;
-            readonly short e_sp;
-            readonly short e_csum;
-            readonly short e_ip;
-            readonly short e_cs;
-            readonly short e_lfarlc;
-            readonly short e_ovno;
-            fixed short e_res[4];
-            readonly short e_oemid;
-            readonly short e_oeminfo;
-            fixed short e_res2[10];
-            public short e_lfanew;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_NT_HEADERS
-        {
-            readonly int Signature;
-            readonly IMAGE_FILE_HEADER FileHeader;
-            public IMAGE_OPTIONAL_HEADER OptionalHeader;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_FILE_HEADER
-        {
-            readonly short Machine;
-            readonly short NumberOfSections;
-            readonly int TimeDataStamp;
-            readonly int PointerToSymbolTable;
-            readonly int NumberOfSymbols;
-            readonly short SizeOfOptionalHeader;
-            readonly short Characteristics;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_OPTIONAL_HEADER
-        {
-            readonly short Magic;
-            readonly byte MajorLinkedVersion;
-            readonly byte MinorLinkedVersion;
-            readonly int SizeOfCode;
-            readonly int SizeOfInitializedData;
-            readonly int SizeOfUInitializedData;
-            readonly int AddressOfEntryPoint;
-            readonly int BaseOfCode;
-            readonly int BaseOfData;
-            readonly int ImageBase;
-            readonly int SectionAlignment;
-            readonly int FileAlignment;
-            readonly short MajorOperatingSystemVersion;
-            readonly short MinororOperatingSystemVersion;
-            readonly short MajorImageVersion;
-            readonly short MinorImageVersion;
-            readonly short MajorSubsystemVersion;
-            readonly short MinorSubsystemVersion;
-            readonly int Win32VersionValue;
-            readonly int SizeOfImage;
-            readonly int SizeOfHeaders;
-            readonly int CheckSum;
-            readonly short Subsystem;
-            readonly short DllCharacteristics;
-            readonly int SizeOfStackReverse;
-            readonly int SizeOfStackCommit;
-            readonly int SizeOfHeapReserve;
-            readonly int SizeOfHeapCommit;
-            readonly int LoaderFlags;
-            readonly int NumberOfRvaAndSizes;
-
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-            public IMAGE_DATA_DIRECTORY[] DataDirectory;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_DATA_DIRECTORY
-        {
-            public int VirtualAddress;
-            public int Size;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct IMAGE_EXPORT_DIRECTORY
-        {
-            readonly int Characteristics;
-            readonly int TimeDateStamp;
-            readonly short MajorVersion;
-            readonly short MinorVersion;
-            readonly int Name;
-            readonly int Base;
-            public int NumberOfFunctions;
-            public int NumberOfNames;
-            public int AddressOfFunctions;
-            public int AddressOfNames;
-            public int AddressOfNameOrdinals;
-        }
-
         /// <summary>
         /// Получает экземпляр класса для работы с виртуальной памятью
         /// </summary>
@@ -135,19 +29,27 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ModuleManager GetModuleManager(this Process process, string moduleName)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
             ProcessModule selectedModule = process.GetModule(moduleName);
 
-            if (selectedModule is null)
+            if (selectedModule == null)
+            {
+                throw new NullReferenceException($"Модуль под именем {moduleName} не найден!");
+            }
+
+            return new ModuleManager(process, selectedModule);
+        }
+
+        /// <summary>
+        /// Получает экземпляр класса для работы с виртуальной памятью выбранного модуля
+        /// </summary>
+        /// <param name="process">Процесс для работы с виртуальной памятью</param>
+        /// <param name="moduleName">Выбранный модуль процесса</param>
+        /// <returns></returns>
+        public static ModuleManager GetModuleManager(this Process process, IntPtr moduleBaseAddress)
+        {
+            ProcessModule selectedModule = process.GetModule(moduleBaseAddress);
+
+            if (selectedModule == null)
             {
                 throw new NullReferenceException("Модуль процесса не найден!");
             }
@@ -181,17 +83,7 @@ namespace System.Diagnostics
         /// </summary>
         /// <param name="process"></param>
         /// <returns></returns>
-        public static PatternManager GetPatternManager(this Process process)
-        {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            MemoryManager memoryManager = process.GetMemoryManager();
-
-            return new PatternManager(process, memoryManager);
-        }
+        public static PatternManager GetPatternManager(this Process process) => new PatternManager(process, process.GetMemoryManager());
 
         /// <summary>
         /// Получает модуль процесса
@@ -201,17 +93,6 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ProcessModule GetModule(this Process process, string moduleName)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
-
             if (string.IsNullOrWhiteSpace(moduleName))
             {
                 throw new ArgumentNullException(nameof(moduleName));
@@ -232,16 +113,6 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ProcessModule GetModule(this Process process, IntPtr hModule)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
             ProcessModule processModule = process.Modules
                 .Cast<ProcessModule>()
                 .FirstOrDefault(currentModule => currentModule.BaseAddress == hModule);
@@ -256,18 +127,7 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ModuleInformationCollection GetModulesAddress(this Process process)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
             ProcessModuleCollection moduleCollection = process.Modules;
-
             ModuleInformation[] addresses = new ModuleInformation[moduleCollection.Count];
 
             for (int index = 0; index < moduleCollection.Count; index++)
@@ -288,29 +148,9 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ModuleFunctionCollection GetModuleFunctions(this Process process, string moduleName)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
+            ModuleManager moduleManager = process.GetModuleManager(moduleName);
 
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
-            if (string.IsNullOrWhiteSpace(moduleName))
-            {
-                throw new ArgumentNullException(nameof(moduleName));
-            }
-
-            ProcessModule module = process.GetModule(moduleName);
-
-            if (module is null)
-            {
-                throw new DllNotFoundException("Could not find library at given address.");
-            }
-
-            return GetModuleFunctions(process, module.BaseAddress);
+            return moduleManager.GetModuleFunctions();
         }
 
         /// <summary>
@@ -321,36 +161,9 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ModuleFunctionCollection GetModuleFunctions(this Process process, IntPtr hModule)
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
+            ModuleManager moduleManager = process.GetModuleManager(hModule);
 
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
-            MemoryManager memory = process.GetMemoryManager();
-
-            IMAGE_DOS_HEADER dosHeader = memory.Read<IMAGE_DOS_HEADER>(hModule);
-
-            IMAGE_NT_HEADERS ntHeader = memory.Read<IMAGE_NT_HEADERS>(hModule + dosHeader.e_lfanew);
-
-            IMAGE_EXPORT_DIRECTORY exportDirectory = memory.Read<IMAGE_EXPORT_DIRECTORY>(hModule + ntHeader.OptionalHeader.DataDirectory[0].VirtualAddress);
-
-            ModuleInformation[] functions = new ModuleInformation[exportDirectory.NumberOfNames];
-
-            for (int index = 0; index < exportDirectory.NumberOfNames; index++)
-            {
-                string functionName = Encoding.UTF8.GetString(memory.ReadBytes((IntPtr)((uint)hModule + (uint)memory.Read<IntPtr>(hModule + (exportDirectory.AddressOfNames + index * 0x4))), X => X == 0).ToArray());
-
-                IntPtr functionAddress = (IntPtr)((uint)hModule + (uint)memory.Read<IntPtr>(hModule + (exportDirectory.AddressOfFunctions + index * 0x4)));
-
-                functions[index] = new ModuleInformation(functionName, functionAddress);
-            }
-
-            return new ModuleFunctionCollection(functions);
+            return moduleManager.GetModuleFunctions();
         }
 
         /// <summary>
@@ -360,24 +173,18 @@ namespace System.Diagnostics
         /// <returns></returns>
         public static ModuleFunctionCollection GetModuleFunctions(this ModuleManager moduleManager)
         {
-            IntPtr moduleAddress = moduleManager.Module.BaseAddress;
-
-            IMAGE_DOS_HEADER dosHeader = moduleManager.Read<IMAGE_DOS_HEADER>(moduleAddress);
-
-            IMAGE_NT_HEADERS ntHeader = moduleManager.Read<IMAGE_NT_HEADERS>(moduleAddress + dosHeader.e_lfanew);
-
-            IMAGE_EXPORT_DIRECTORY exportDirectory = moduleManager.Read<IMAGE_EXPORT_DIRECTORY>(moduleAddress + ntHeader.OptionalHeader.DataDirectory[0].VirtualAddress);
-
+            ImageDosHeader dosHeader = moduleManager.Read<ImageDosHeader>(IntPtr.Zero);
+            ImageNtHeaders ntHeader = moduleManager.Read<ImageNtHeaders>((IntPtr)dosHeader.e_lfanew);
+            ImageExportDirectory exportDirectory = moduleManager.Read<ImageExportDirectory>((IntPtr)ntHeader.OptionalHeader.DataDirectory[0].VirtualAddress);
             ModuleInformation[] functions = new ModuleInformation[exportDirectory.NumberOfNames];
 
             for (int index = 0; index < exportDirectory.NumberOfNames; index++)
             {
-                byte[] functionNameBytes = moduleManager
-                    .ReadBytes((IntPtr)((uint)moduleAddress + (uint)moduleManager.Read<IntPtr>(moduleAddress + (exportDirectory.AddressOfNames + index * 0x4))), X => X != 0);
+                string functionName = Encoding.UTF8
+                    .GetString(moduleManager.ReadBytes(moduleManager.Read<IntPtr>((IntPtr)(exportDirectory.AddressOfNames + index * 0x4)), X => X == 0)
+                .ToArray());
 
-                string functionName = Encoding.UTF8.GetString(functionNameBytes);
-
-                IntPtr functionAddress = (IntPtr)((uint)moduleAddress + (uint)moduleManager.Read<IntPtr>(moduleAddress + (exportDirectory.AddressOfFunctions + index * 0x4)));
+                IntPtr functionAddress = (IntPtr)((uint)moduleManager.Module.BaseAddress + (uint)moduleManager.Read<IntPtr>((IntPtr)(exportDirectory.AddressOfFunctions + index * 0x4)));
 
                 functions[index] = new ModuleInformation(functionName, functionAddress);
             }
@@ -395,23 +202,12 @@ namespace System.Diagnostics
         /// <returns></returns>
         internal static TException ShowException<TException>(this Process process, IntPtr address, string message) where TException : Exception
         {
-            if (process is null)
-            {
-                throw new ArgumentNullException(nameof(process));
-            }
-
-            if (process.HasExited)
-            {
-                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
-            }
-
             TException exception = typeof(TException)
                 .GetConstructor(new Type[] { typeof(string) })
                 .Invoke(new object[] { message }) as TException;
 
             PageManager pageManager = process.GetPageManager();
-
-            MEMORY_BASIC_INFORMATION pageInformation = pageManager.GetPageInformation(address);
+            MemoryBasicInformation pageInformation = pageManager.GetPageInformation(address);
 
             exception.Data.Add("Process", process.ProcessName);
             exception.Data.Add("Address", address);
@@ -423,6 +219,19 @@ namespace System.Diagnostics
             exception.Data.Add("MemoryType", pageInformation.Type);
 
             return exception;
+        }
+
+        internal static void CheckProcess(Process process)
+        {
+            if (process == null)
+            {
+                throw new ArgumentNullException(nameof(process));
+            }
+
+            if (process.HasExited)
+            {
+                throw new ApplicationException($"Процесс {process.ProcessName} является завершенным");
+            }
         }
 
         /// <summary>
